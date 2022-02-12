f85386d41ba129721dbb2e7e0f01227147e79d85
Nimet Ozkan <melaht2016@icloud.com>
2022-02-12 15:09:50 -0600

    added a py file for db and created/filled tables

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-from flask import Flask, render_template, request, flash
-import requests, json
-import sqlite3
+from flask import Flask, render_template, request, flash, g, session
+import sqlite3, random
 
 # Create instance of Flask by calling its class constructor
 app = Flask(__name__)
 app.secret_key = "12ev@0947!fbjs8$32jhbd_sjhAdhc%mhn_773gvsahG/HSI*IH_jbs"
-
-# Get data from the Mock API using GET HTTP method
-# response data type will be byte
-response = requests.get("https://mock-starbucs-api.herokuapp.com/drinks")
-
-# Check if the request was successful
-if response.status_code == 200:
-    print('Success!')
-elif response.status_code == 404:
-    print('Not Found.')
-
-
-# Convert bytes into a JSON list data
-drinks_json = response.json()
-
-# Create an empty database 
-connection = sqlite3.connect('drinks_list.db')
 
 # Main page
 @app.route('/', methods=['GET'])
@@ -35,10 +17,6 @@ def hello():
     flash('Hi ' + str(request.form['name_input']).capitalize() + "! Time to choose your drink!")
     return render_template("index.html")
 
-
-# Do not forget to save (commit) and close the database connection
-connection.commit()
-connection.close()
 
 # run app
 if __name__ == '__main__':
---
+++ drinks_list.py
@@ -0,0 +1,32 @@
+import sqlite3, requests
+
+# Get data from the Mock API using GET HTTP method
+# response data type will be byte
+response = requests.get("https://mock-starbucs-api.herokuapp.com/drinks")
+
+# Check if the request was successful
+if response.status_code == 200:
+    print('Success!')
+elif response.status_code == 404:
+    print('Not Found.')
+
+
+# Convert bytes into a JSON list data
+drinks = response.json()
+
+# Create an empty database 
+connection = sqlite3.connect('drinks_list.db')
+
+# communication with database via cursor with SQL commands
+cursor = connection.cursor()
+
+# Create table/s for our database
+cursor.execute("create table drinks (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+for i in range(len(drinks)):
+  cursor.execute("insert into drinks (name) values (?)",[drinks[i]])
+  print("added ", drinks[i])
+
+
+# Do not forget to save (commit) and close the database connection
+connection.commit()
+connection.close()
